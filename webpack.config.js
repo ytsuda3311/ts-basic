9e1c3e1c35821b27a1f9b9071c5f76be112fc121
unknown <ytsuda3311@gmail.com>
2023-04-06 00:34:44 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    bundle: "./src/index.ts",
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        loader: "ts-loader",
+        test: /\.ts$/,
+      },
+    ],
+  },
+};
